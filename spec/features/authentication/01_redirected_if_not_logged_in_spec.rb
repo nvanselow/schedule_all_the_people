5e08914580118f7f0f9813b343aef33cdd1a66bb
Nick(Mac) <nvanselow@hotmail.com>
2016-06-29 00:03:34 -0400

    Start auth check tests

---
+++ spec/features/authentication/01_redirected_if_not_logged_in_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+feature "can't visit auth protected pages if not logged in" do
+  scenario "logged out user can visit homepage" do
+    visit root_path
+
+    expect(page).to have_content("Please sign in using your Google account to start using this web app.")
+    expect(page).not_to have_content("Events")
+    expect(page).not_to have_content("Groups")
+  end
+
+  scenario "attempts to visit groups page" do
+    visit groups_path
+    check_auth_error
+  end
+
+  scenario "attempts to create a new group" do
+    visit new_group_path
+    check_auth_error
+  end
+
+  scenario "attempts to edit a group" do
+    group = FactoryGirl.create(:group)
+    visit edit_group_path(group)
+    check_auth_error
+  end
+
+  scenario "attempts to view a group" do
+    group = FactoryGirl.create(:group)
+    visit group_path(group)
+    check_auth_error
+  end
+
+  scenario "attempts to view events" do
+    visit events_path
+    check_auth_error
+  end
+
+  scenario "attempts to create a new event" do
+    visit new_event_path
+    check_auth_error
+  end
+
+  scenario "attempts to edit an event" do
+    event = FactoryGirl.create(:event)
+    visit edit_event_path(event)
+    check_auth_error
+  end
+
+  scenario "attempts to view an event" do
+    event = FactoryGirl.create(:event)
+    visit event_path(event)
+    check_auth_error
+  end
+end
+
+def check_auth_error
+  expect(page).to have_content("Please login to visit that page.")
+  expect(current_path).to eq(root_path)
+end
