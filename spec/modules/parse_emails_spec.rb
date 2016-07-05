c4f9c367e9ec8dc62bb381f78388974737eece28
Nick(Mac) <nvanselow@hotmail.com>
2016-07-05 19:58:15 -0400

    Fix folder reference

--- spec/modules/parse_emails_spec.rb
+++ spec/modules/parse_emails_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../../lib/modules/email_list'
 
-describe EmailList do
+describe Modules::EmailList do
   let(:test_email) { "test1@test.com" }
   let(:emails) { "
       #{test_email}, test2@test.com, test3@test.com
@@ -14,25 +14,25 @@ describe EmailList do
 
   describe ".parse" do
     it "accepts an comma separated string of emails" do
-      expect { EmailList.parse(emails) }.not_to raise_error
+      expect { Modules::EmailList.parse(emails) }.not_to raise_error
     end
 
     it "separates the list of email in to an array" do
-      email_array = EmailList.parse(emails)
+      email_array = Modules::EmailList.parse(emails)
 
       expect(email_array.size).to eq(3)
       expect(email_array).to include(test_email)
     end
 
     it "accurately separates a list separated by semicolons" do
-      email_array = EmailList.parse(emails_with_semicolons)
+      email_array = Modules::EmailList.parse(emails_with_semicolons)
 
       expect(email_array.size).to eq(3)
       expect(email_array).to include(test_email)
     end
 
     it "accurately separates emails even if there is no space after commas" do
-      email_array = EmailList.parse(emails_without_spaces)
+      email_array = Modules::EmailList.parse(emails_without_spaces)
 
       expect(email_array.size).to eq(3)
       expect(email_array).to include(test_email)
